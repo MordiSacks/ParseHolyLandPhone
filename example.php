a10346908a9638ff8bbaba197ce2a0a186b81a27
Mordi Sacks <mordi.sacks@triple.co.il>
2017-10-25 17:49:06 +0300

    Added example

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+
+use ParseHolyLandPhone\ParsePhone;
+
+require_once('vendor/autoload.php');
+
+// via new instance
+$number = new ParsePhone('025121234');
+
+// via static method
+$number = ParsePhone::create('025121234');
+
+$number->isValid();
+
+$number->isNotValid();
